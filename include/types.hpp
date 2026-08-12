4c155ba02176868bb69811f732c9aeccb910d5e9
dusty <dmitriysholota@gmail.com>
2026-08-13 00:26:01 +0300

    Обьявлены и прописаны типы структуры для реализации проекта из geojson

--- include/types.hpp
+++ include/types.hpp
@@ -1,8 +1,25 @@
 //
-// Created by Sholota Dmitriy on 12.08.2026.
-//
+// Файл, которые описывает типы обьектов которые будут применяется во всем проекте
+// Точки Линии путей Графы
+#pragma once
+#include <vector>
+#include <string>
 
-#ifndef TYPES_H
-#define TYPES_H
+struct Point {   // Структура обработка координат (точки или конца вектора) Узлы графа
+    double lon;  // долгота
+    double lat;  // широта
 
-#endif //TYPES_H
+    Point (): lon(0), lat(0) {}
+    Point (double lon, double lat): lon(lon), lat(lat) {}
+
+    bool operator<(const Point& other) const {
+        if (lon != other.lon) return lon < other.lon;
+        return lat < other.lat;
+    }
+    bool operator==(const Point& other) const {
+        return lon == other.lon && lat == other.lat;
+    }
+};
+
+
+using Line = std::vector<Point>;  //Псевдониим std::vector<Point>
